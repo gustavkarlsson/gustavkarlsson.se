8ea4d614711bbf054f1c3c27a450e25035ca7ae8
Gustav Karlsson <gustav.karlsson@gmail.com>
2014-08-02 00:27:25 +0200

    Beautified source

--- js/main.js
+++ js/main.js
@@ -1,127 +1,129 @@
 // Plugins
 
-var setActiveMenuItemPlugin = (function() {
-    var lastVisitedId;
+var setActiveMenuItemPlugin = (function () {
+	var lastVisitedId;
 
-    function isScrollbarAtTop() {
-        return $(window).scrollTop() <= 0;
-    }
+	function isScrollbarAtTop() {
+		return $(window).scrollTop() <= 0;
+	}
 
-    function isScrollbarAtBottom() {
-        var totalHeight = $("body").height();
-        var windowHeight = $(window).height();
-        var windowTop = $(window).scrollTop();
-        return windowTop + windowHeight >= totalHeight;
-    }
+	function isScrollbarAtBottom() {
+		var totalHeight = $("body").height();
+		var windowHeight = $(window).height();
+		var windowTop = $(window).scrollTop();
+		return windowTop + windowHeight >= totalHeight;
+	}
 
-    function getCenterElement(elements, offset) {
-        // Find vertical center position of window
-        var windowTop = $(window).scrollTop() + offset;
-        var windowHeight = $(window).height() - offset;
-        var windowCenter = windowTop + (windowHeight / 2);
+	function getCenterElement(elements, offset) {
+		// Find vertical center position of window
+		var windowTop = $(window).scrollTop() + offset;
+		var windowHeight = $(window).height() - offset;
+		var windowCenter = windowTop + (windowHeight / 2);
 
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            var elementHeight = element.outerHeight();
-            var elementTop = element.offset().top;
-            var elementBottom = elementTop + elementHeight;
-            if (elementTop <= windowCenter && windowCenter <= elementBottom) {
-                return element;
-            }
-        }
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var elementHeight = element.outerHeight();
+			var elementTop = element.offset().top;
+			var elementBottom = elementTop + elementHeight;
+			if (elementTop <= windowCenter && windowCenter <= elementBottom) {
+				return element;
+			}
+		}
 
-        return null;
-    }
-    
-    return {
-        exec: function(sections) {
-            var currentElement;
-                
-            if (isScrollbarAtTop()) {
-                currentElement = sections[0];
-            } else if (isScrollbarAtBottom()) {
-                currentElement = sections[sections.length - 1];
-            } else {
-                currentElement = getCenterElement(sections, header.outerHeight());
-            }
-            
-            var currentId = currentElement && currentElement.length ? currentElement[0].id : "";
+		return null;
+	}
 
-            // Check if current ID has changed
-            if (lastVisitedId !== currentId) {
-                // Update last ID
-                lastVisitedId = currentId;
-                // Set/remove active class
-                navLinks.removeClass('active');
-                navLinks.filter('[href=#' + currentId + ']').addClass('active');
-            }
-        }
-    }
+	return {
+		exec: function (sections) {
+			var currentElement;
+
+			if (isScrollbarAtTop()) {
+				currentElement = sections[0];
+			} else if (isScrollbarAtBottom()) {
+				currentElement = sections[sections.length - 1];
+			} else {
+				currentElement = getCenterElement(sections, header.outerHeight());
+			}
+
+			var currentId = currentElement && currentElement.length ? currentElement[0].id : "";
+
+			// Check if current ID has changed
+			if (lastVisitedId !== currentId) {
+				// Update last ID
+				lastVisitedId = currentId;
+				// Set/remove active class
+				navLinks.removeClass('active');
+				navLinks.filter('[href=#' + currentId + ']').addClass('active');
+			}
+		}
+	}
 })();
 
-var cvHandler = (function() {
-    var status = "fixed";
-	
+var cvHandler = (function () {
+	var status = "fixed";
+
 	function setFixed() {
 		status = "fixed";
 	}
-	
+
 	function animateHeight(height) {
-		cvWrapper.stop().animate({height: height}, setFixed);
+		cvWrapper.stop().animate({
+			height: height
+		}, setFixed);
 	}
-	
+
 	function show(cvWrapper, cvContent) {
 		animateHeight(cvContent.outerHeight());
 		status = "showing";
 	}
-	
+
 	function hide(cvWrapper) {
 		animateHeight(0);
 		status = "hiding";
 	}
-    
-    return {
-        toggle: function(cvWrapper, cvContent) {
-            if (status === "showing" || (status === "fixed" && cvWrapper.height() > 0)) {
+
+	return {
+		toggle: function (cvWrapper, cvContent) {
+			if (status === "showing" || (status === "fixed" && cvWrapper.height() > 0)) {
 				hide(cvWrapper);
 			} else {
 				show(cvWrapper, cvContent);
 			}
-        },
-		updateSize: function(cvWrapper, cvContent) {
-            if (status === "showing" || (status === "fixed" && cvWrapper.height() > 0)) {
+		},
+		updateSize: function (cvWrapper, cvContent) {
+			if (status === "showing" || (status === "fixed" && cvWrapper.height() > 0)) {
 				cvWrapper.height(cvContent.outerHeight());
 			} else {
 				cvWrapper.height(0);
 			}
-        }
-    }
+		}
+	}
 })();
 
 // Functions
 
 function scrollToAnchor(link, event, offsetElement) {
-    if (offsetElement.css('position') === 'fixed') {
-        offset = offsetElement.outerHeight();
-    } else {
-        offset = 0;
-    }
-    var anchor = link.attr('href');
-    var offsetTop = $(anchor).offset().top - offset + 1;
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 300);
+	if (offsetElement.css('position') === 'fixed') {
+		offset = offsetElement.outerHeight();
+	} else {
+		offset = 0;
+	}
+	var anchor = link.attr('href');
+	var offsetTop = $(anchor).offset().top - offset + 1;
+	$('html, body').stop().animate({
+		scrollTop: offsetTop
+	}, 300);
 
-    if (history.pushState) {
-        // Prevents flicker
-        history.pushState(null, null, anchor);
-    } else {
-        // Fallback for some browsers (causes minor flicker)
-        location.hash = anchor;
-    }
+	if (history.pushState) {
+		// Prevents flicker
+		history.pushState(null, null, anchor);
+	} else {
+		// Fallback for some browsers (causes minor flicker)
+		location.hash = anchor;
+	}
 
-    // Prevent default behavior to enable scrolling
-    event.preventDefault();
+	// Prevent default behavior to enable scrolling
+	event.preventDefault();
 }
 
 // Static members
@@ -129,10 +131,10 @@ function scrollToAnchor(link, event, offsetElement) {
 var header = $("header");
 var navLinks = header.find("nav a");
 var namedAnchors = navLinks.map(function () {
-    var anchor = $($(this).attr("href"));
-    if (anchor.length === 1 && anchor.selector.charAt(0) === "#") {
-        return anchor;
-    }
+	var anchor = $($(this).attr("href"));
+	if (anchor.length === 1 && anchor.selector.charAt(0) === "#") {
+		return anchor;
+	}
 });
 var cvWrapper = $("#cv-wrapper");
 var cvContent = $("#cv-content");
@@ -141,7 +143,7 @@ var cvToggle = $("#cv-arrow");
 // Binding
 
 navLinks.click(function (event) {
-    scrollToAnchor($(this), event, header);
+	scrollToAnchor($(this), event, header);
 });
 
 cvToggle.click(function (event) {
@@ -149,16 +151,16 @@ cvToggle.click(function (event) {
 });
 
 $(window).scroll(function () {
-    setActiveMenuItemPlugin.exec(namedAnchors);
+	setActiveMenuItemPlugin.exec(namedAnchors);
 });
 
 $(window).resize(function () {
-    setActiveMenuItemPlugin.exec(namedAnchors);
+	setActiveMenuItemPlugin.exec(namedAnchors);
 	cvHandler.updateSize(cvWrapper, cvContent);
 });
 
 $(document).ready(function () {
-    setActiveMenuItemPlugin.exec(namedAnchors); // Execute once
+	setActiveMenuItemPlugin.exec(namedAnchors); // Execute once
 });
 
 // Initialization
